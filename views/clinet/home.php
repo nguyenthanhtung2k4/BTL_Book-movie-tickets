40da4fc61e48e16ddae41332fc8771cffdf38f48
JUSST <88431113+nguyenthanhtung2k4@users.noreply.github.com>
2025-10-27 17:12:34 +0700

    create php

---
+++ views/clinet/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Home</title>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <!-- Thư viện Icon (Lucide Icons) -->
+      <script src="https://unpkg.com/lucide@latest"></script>
+      <!-- Thư viện Font (Inter) -->
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="./asset/css/style.css">
+</head>
+<body>
+      
+</body>
+</html>
